fe57ee7ae62bc821149dc5e826288edccf5a4514
exoticsolutions <rafajr1263@gmail.com>
2024-06-28 15:03:37 -0400

    o

--- my-project/src/componets/swap/Swapper.jsx
+++ my-project/src/componets/swap/Swapper.jsx
@@ -294,50 +294,53 @@ function Swapper(props) {
         />
       </div>
       <div className="grid grid-rows-1 gap-y-1 p-1">
-        <div className="border p-2 rounded-lg border-neutral-700 text-white font-mono font-bold text-[12px]">
+        <div className="border p-2 rounded-lg border-neutral-700 text-white font-mono font-bold text-[10px]">
           <div className="flex justify-between">
             <h3>{shouldMint ? "You Buy" : "You Sell"}</h3>
-
-            <h3 className="text-[8px]">Share balance:{" " + shareBalance}</h3>
           </div>
 
-          <input
-            type="text"
-            className="w-[350px] bg-transparent border border-transparent outline-none"
-            onChange={(e) => {
-              if (!isNaN(Number(e.target.value))) {
-                setInput(Number(e.target.value));
-              }
-            }}
-            value={input || 0}
-          />
-          <div>
+          <div className="flex justify-between">
+            <input
+              type="text"
+              className="w-[150px] bg-transparent border border-transparent outline-none text-[14px]"
+              onChange={(e) => {
+                if (!isNaN(Number(e.target.value))) {
+                  setInput(Number(e.target.value));
+                }
+              }}
+              value={input || 0}
+            />
+            <button
+              className="border w-[150px] rounded-md border-neutral-600 bg-stone-800 hover:text-stone-500"
+              onClick={() => document.getElementById("my_modal_1").showModal()}
+            >
+              <div className="flex justify-between gap-1 p-0.5">
+                <div className="flex justify-start gap-1">
+                  <img
+                    src={currentShare?.ftPfpUrl}
+                    alt=""
+                    className="w-3 h-3 mt-[3px] ms-1 rounded-full"
+                  />
+                  <h3 className="whitespace-nowrap truncate text-[8px] mt-0.5">
+                    {currentShare?.ftName}
+                  </h3>
+                </div>
+                <img
+                  src="https://enterprisefilmsllc.com/wp-content/uploads/2018/07/white-down-arrow-png-2.png"
+                  alt=""
+                  className="w-3 h-3 mt-1"
+                />
+              </div>
+            </button>
+          </div>
+
+          <div></div>
+          <div className="flex justify-between mt-2">
             <h3 className="text-[7px]">
               {"≈ " + Number(currentTotal * ethPrice).toFixed(2) + " USD"}
             </h3>
+            <h3 className="text-[8px]">Share balance:{" " + shareBalance}</h3>
           </div>
-          <button
-            className="border w-[200px] rounded-md border-neutral-600 bg-stone-800 mt-1 hover:text-stone-500"
-            onClick={() => document.getElementById("my_modal_1").showModal()}
-          >
-            <div className="flex justify-between gap-1 p-0.5">
-              <div className="flex justify-start gap-1">
-                <img
-                  src={currentShare?.ftPfpUrl}
-                  alt=""
-                  className="w-3 h-3 mt-[3px] ms-1 rounded-full"
-                />
-                <h3 className="whitespace-nowrap truncate text-[8px] mt-0.5">
-                  {currentShare?.ftName}
-                </h3>
-              </div>
-              <img
-                src="https://enterprisefilmsllc.com/wp-content/uploads/2018/07/white-down-arrow-png-2.png"
-                alt=""
-                className="w-3 h-3 mt-1"
-              />
-            </div>
-          </button>
         </div>
         <div className="flex justify-center">
           <div
@@ -357,41 +360,42 @@ function Swapper(props) {
           </div>
         </div>
 
-        <div className="border border-neutral-700 rounded-lg text-white font-mono font-bold text-[12px] p-2">
+        <div className="border border-neutral-700 rounded-lg text-white font-mono font-bold text-[10px] p-2">
           <div className="flex justify-between">
             <h3>{shouldMint ? "You Pay" : "You Recieve"}</h3>
-
-            <h3 className="text-[8px]">ETH balance:{" " + ethBalance || 0}</h3>
           </div>
-          <input
-            type="text"
-            className="w-[350px] bg-transparent border border-transparent outline-none"
-            value={currentTotal || 0}
-          />
-          <div>
+          <div className="flex justify-between">
+            <input
+              type="text"
+              className="w-[150px] bg-transparent border border-transparent outline-none text-[14px]"
+              value={currentTotal || 0}
+            />
+            <button className="border w-[150px] rounded-md border-neutral-600 bg-stone-800 mt-1 hover:text-stone-500">
+              <div className="flex justify-between gap-1 p-0.5">
+                <div className="flex justify-start gap-1">
+                  <img
+                    src={AvaliablePairs[0]?.imgUrl}
+                    alt=""
+                    className="w-3 h-3 mt-[3px] ms-1"
+                  />
+                  <h3 className="text-[8px] mt-[3px]">
+                    {AvaliablePairs[0]?.name}
+                  </h3>
+                </div>
+                <img
+                  src="https://enterprisefilmsllc.com/wp-content/uploads/2018/07/white-down-arrow-png-2.png"
+                  alt=""
+                  className="w-3 h-3 mt-1"
+                />
+              </div>
+            </button>
+          </div>
+          <div className="flex justify-between mt-2">
             <h3 className="text-[7px]">
               {"≈ " + Number(currentTotal * ethPrice).toFixed(2) + " USD"}
             </h3>
+            <h3 className="text-[8px]">ETH balance:{" " + ethBalance || 0}</h3>
           </div>
-          <button className="border w-[200px] rounded-md border-neutral-600 bg-stone-800 mt-1 hover:text-stone-500">
-            <div className="flex justify-between gap-1 p-0.5">
-              <div className="flex justify-start gap-1">
-                <img
-                  src={AvaliablePairs[0]?.imgUrl}
-                  alt=""
-                  className="w-3 h-3 mt-[3px] ms-1"
-                />
-                <h3 className="text-[8px] mt-[3px]">
-                  {AvaliablePairs[0]?.name}
-                </h3>
-              </div>
-              <img
-                src="https://enterprisefilmsllc.com/wp-content/uploads/2018/07/white-down-arrow-png-2.png"
-                alt=""
-                className="w-3 h-3 mt-1"
-              />
-            </div>
-          </button>
         </div>
         <div className="">
           <div className="p-2 text-[8px] mb-2">
