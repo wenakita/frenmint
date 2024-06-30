1872f65118d599547418d5182991abdd906a1f06
exoticsolutions <rafajr1263@gmail.com>
2024-06-29 22:11:32 -0400

    o

--- my-project/src/componets/NewHome/NewNavigation.jsx
+++ my-project/src/componets/NewHome/NewNavigation.jsx
@@ -203,54 +203,18 @@ function NewNavigation(props) {
                 <CiWallet className="text-[20px]" />
               </Link>
             </button> */}
-            <div className="dropdown relative">
-              {currentUser && (
-                <div
-                  tabIndex={0}
-                  role="button"
-                  className="btn btn-ghost btn-circle"
-                >
-                  <img
-                    src={
-                      currentUser?.img_url ||
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk3BEiUjewgdlaP0T1JRZXBbeB4RjAXnz_Tg&s"
-                    }
-                    alt=""
-                    className="size-6 rounded-full mt-0.5"
-                  />
-                </div>
-              )}
-              <ul className="menu menu-sm dropdown-content mt-3 z-10 p-6 shadow bg-neutral-900 rounded-box w-52 font-bold absolute right-0">
-                <li>
-                  <div className="text-start text-xs text-gray-200 font-bold font-mono truncate">
-                    Welcome back {currentUser?.username}
-                  </div>
-                </li>
-                <li>
-                  <Link
-                    to="/new"
-                    className="block border border-transparent hover:bg-neutral-900 w-full py-2 px-4"
-                    onClick={() => {
-                      // Close the dropdown if needed
-                      navigate("/new");
-                    }}
-                  >
-                    <div className="flex items-center gap-2">
-                      <CiWallet className="text-2xl" />
-                      <h3 className=" text-[12px]">Wallet</h3>
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <div className="flex border border-1 border-b-0 border-l-0 border-r-0 rounded-none border-neutral-800">
-                    <FaEthereum className="mt-1 text-xs text-gray-500" />
-                    <h3 className="text-white text-xs font-mono mt-1">
-                      {ethBalance}
-                    </h3>
-                  </div>
-                </li>
-              </ul>
-            </div>
+            {currentUser && (
+              <Link to={"/new"}>
+                <img
+                  src={
+                    currentUser?.img_url ||
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk3BEiUjewgdlaP0T1JRZXBbeB4RjAXnz_Tg&s"
+                  }
+                  alt=""
+                  className="size-6 rounded-full mt-0.5 me-2"
+                />
+              </Link>
+            )}
           </div>
         ) : null}
       </div>
